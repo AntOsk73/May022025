e15989ff79c431231592e6f05f9f163b21269898
AntOsk73 <antoine.osky@bnc.ca>
2025-05-02 19:20:17 -0400

    Update ressource_group.tf

--- ressource_group.tf
+++ ressource_group.tf
@@ -1,7 +1,4 @@
-ressource
-"azurerm_resource_group"
-"antoineform"{
-  name= 
-  "mcit_resource_group_antoineosky"
+ressource "azurerm_resource_group" "antoineform"{
+  name= "mcit_resource_group_antoineosky"
   location="canadacentral"
   }
