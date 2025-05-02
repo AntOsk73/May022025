7ed255892795fde442dabc00a2de0c36d1f08539
AntOsk73 <antoine.osky@bnc.ca>
2025-05-02 19:49:34 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -12,7 +12,7 @@ terraform{
       version=">3.70.0"#this version is for azurerm, NOT terraform version
        }
      }
-     required_version="1.4.0"this version is for Terraform version, NOT azurerm
+     required_version=">1.4.0"this version is for Terraform version, NOT azurerm
      }
 
   
