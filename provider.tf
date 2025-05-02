383b2b0bbf6b7242c0ed812c135a30246eef0ff4
AntOsk73 <antoine.osky@bnc.ca>
2025-05-02 19:02:27 -0400

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,18 @@
+provider "azurerm"{
+  features{}
+  subscription_id=var.subscription+id
+  client_id=var.client_id
+  client_secret=var.client_secret
+  tenant_id=var.tenant_id
+}
+terraform{
+  required_providers{
+    azurerm={
+      source="hashicorp/azurerm"
+      version= ">3.70.0"#this version is for azurerm, NOT terraform version
+       }
+     }
+     required_version="1.4.0"this version is for Terraform version, NOT azurerm
+     }
+
+  
