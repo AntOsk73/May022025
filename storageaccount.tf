ebf040b230f607a0ce78800f94ae1083388c539c
AntOsk73 <antoine.osky@bnc.ca>
2025-05-05 20:22:53 -0400

    Update storageaccount.tf

--- storageaccount.tf
+++ storageaccount.tf
@@ -3,7 +3,7 @@ resource "azurerm_storage_account" "mcitstorage" {
   resource_group_name      = azurerm_resource_group.antoineoskyform.name
   location                 = azurerm_resource_group.antoineoskyform.location
   account_tier             = var."account_tier"
-  account_replication_type = "LRS"
+  account_replication_type = var."account_replication_type"
 
   tags = {
     environment = "dev"
