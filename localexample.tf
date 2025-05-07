8ba105849714afc4d197877b5d994aecd4a1418d
AntOsk73 <antoine.osky@bnc.ca>
2025-05-07 18:44:41 -0400

    Update localexample.tf

--- localexample.tf
+++ localexample.tf
@@ -3,3 +3,7 @@ locals{
   forum="bessamemucho"
   lengthsa=length(local.servicename)
 }
+
+winterlistOfSports=["icehockey","snowboarding","iceskating"]
+  total_output = ["150", "150", "150"]
+  characters = ["luke", "yoda", "darth"]
