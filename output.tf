46b52e0d6c0c3e6dd3e28a7590e3684143c42ccd
AntOsk73 <antoine.osky@bnc.ca>
2025-05-07 19:37:13 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -27,3 +27,7 @@ output "wintersportslist"{
 output "total_sum" {
   value = sum([for total in local.total_output : tonumber(total)])
 }
+
+output "character_enemy_output" {
+  value = local.character_enemy_map
+}
