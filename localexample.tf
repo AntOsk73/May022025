ecae52d368658dc5dec35c6fb70271b67b68fd45
AntOsk73 <antoine.osky@bnc.ca>
2025-05-07 19:32:53 -0400

    Update localexample.tf

--- localexample.tf
+++ localexample.tf
@@ -6,4 +6,8 @@ locals{
 winterlistOfSports=["weightlifting","swimming","jogging"]
   total_output = ["1973", "2025", "2035"]
   characters = ["luke", "yoda", "darth"]
+  enemies_destroyed = [4252, 900, 20000056894]
+  character_enemy_map =   { for character in local.characters: # Convert character list to a set
+      character => local.enemies_destroyed
+
 }
