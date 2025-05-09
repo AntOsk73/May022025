065a70547613fa24d15a8553efc561a4574aa4d1
AntOsk73 <antoine.osky@bnc.ca>
2025-05-09 18:26:57 -0400

    Update localexample.tf

--- localexample.tf
+++ localexample.tf
@@ -16,6 +16,5 @@ winterlistOfSports=["weightlifting","swimming","jogging"]
   enemies_destroyed = [4252, 900, 20000056894]
   character_enemy_map =   { for character in local.characters: # Convert character list to a set
       character => local.enemies_destroyed
-
  }
 }
