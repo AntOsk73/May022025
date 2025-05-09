63c4ebfdce14b0c1c9a8bd757376d3f50406d67e
AntOsk73 <antoine.osky@bnc.ca>
2025-05-09 18:30:40 -0400

    Update localexample.tf

--- localexample.tf
+++ localexample.tf
@@ -17,4 +17,12 @@ winterlistOfSports=["weightlifting","swimming","jogging"]
   character_enemy_map =   { for character in local.characters: # Convert character list to a set
       character => local.enemies_destroyed
  }
+
+listoffavoritenumbers=["My favorite numbers are"
+total_output = ["42", "77", "13", "98", "3", "64", "55", "27", "89", "6"]
 }
+
+}
+
+
+
