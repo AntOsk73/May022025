4cf59ebff876a3ae17ff55c0956bfcfa6d3d7c25
AntOsk73 <antoine.osky@bnc.ca>
2025-05-09 18:49:09 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -41,7 +41,7 @@ output "total_numbers" {
 }
 
 output "total_liststring" {
-  value = sum([for total in local.number : tonumber(total)])
+  value = sum([for total in var.number : tonumber(total)])
 }
 
 
