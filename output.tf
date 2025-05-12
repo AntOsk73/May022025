b39123cfcc706ec259083b61072c47fd86c56bb3
AntOsk73 <antoine.osky@bnc.ca>
2025-05-12 19:12:07 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,60 +1,61 @@
-output "print"{
+/*output "print"{
     value = local.servicename
-}
-output "secondprint"{
-    value = local.forum
-}
- 
-output "lengthsa"{
-    value=local.lengthsa
-}
+}*/
 
-output "wintersportslist"{
+/*output "secondprint"{
+    value = local.forum
+}*/
+ 
+/*output "lengthsa"{
+    value=local.lengthsa
+}*/
+
+/*output "wintersportslist"{
     value=[for sport in local.winterlistOfSports:sport]
    
-}
+}*/
 
- output "total_output"{
+ /*output "total_output"{
    value=[for sport in local.total_output:sport]
 
-}
+}*/
 
-   output "characterst"{
+   /*output "characterst"{
    value=[for sport in local.characters:sport]
 
-}
+}*/
 
-output "total_sum" {
+/*output "total_sum" {
   value = sum([for total in local.total_output : tonumber(total)])
-}
+}*/
 
-output "character_enemy_output" {
+/*output "character_enemy_output" {
   value = local.character_enemy_map
-}
+}*/
 
-output "top5restaurants_output"{
+/*output "top5restaurants_output"{
  value = local.character_food_map
-}
+}*/
 
-output "total_numbers" {
+/*output "total_numbers" {
   value = sum([for total in local.total_numbers : tonumber(total)])
-}
+}*
 
-output "total_liststring" {
+/*output "total_liststring" {
   value = sum([for total in var.number : tonumber(total)])
-}
+}*/
 
-output "uppercased_sentence" {
+/*output "uppercased_sentence" {
   value = upper(var.sentence)
-}
+}*/
 
-output "lowercased_word" {
+/*output "lowercased_word" {
   value = lower(var.word)
-}
+}*/
 
-output "extract_canada" {
+/*output "extract_canada" {
   value = substr(var.full_text, 26, 6)
-}
+}*/
 /*
 "I love the country called Canada, it is awesome"
 substr(string, offset, length)
