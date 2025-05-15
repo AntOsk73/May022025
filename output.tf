ec20eb0e4834503f87ce66b81bc10c689bec7ed4
AntOsk73 <antoine.osky@bnc.ca>
2025-05-14 20:38:29 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -147,3 +147,8 @@ output "excercise_4" {
   value = element(["Antoine", "Ovadia", "Onome", "Sean", "Negar"], 4)
 }
 
+output "excercise_5" {
+  value = element(["Antoine", "Ovadia", "Onome", "Sean", "Negar"], 3)
+}
+
+
