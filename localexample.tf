cfa0c82855225bee0e72360bbc12973a1a0256c2
AntOsk73 <antoine.osky@bnc.ca>
2025-05-16 18:30:01 -0400

    Update localexample.tf

--- localexample.tf
+++ localexample.tf
@@ -38,7 +38,7 @@ sentence_5 = "Bagels in Montreal are boiled, not baked."
 words     = split(" ", local.sentence_5)
 third_word = local.words[2] # Index starts at 0
 
-servicename = "cloudCadabra"
+servicename1 = "cloudCadabra"
 env = "non-prod"
 alltogether = "${local.cloudCadabra} + {local.env}"
 
