03ba640ef1d4b592942d664840d3c5763c96b343
AntOsk73 <antoine.osky@bnc.ca>
2025-05-21 19:26:08 -0400

    Update mcityaml.tf

--- mcityaml.tf
+++ mcityaml.tf
@@ -1,1 +1,25 @@
+provider "azurerm" {
+  features {}
+}
 
+resource "azurerm_resource_group" "mcitdevrm" {
+  name     = "example-resources"
+  location = "canada central"
+}
+
+resource "azurerm_service_plan" "mcitdevrm" {
+  name                = "mcitdevrm"
+  resource_group_name = azurerm_resource_group.mcitdevrm.name
+  location            = azurerm_resource_group.mcitdevrm.location
+  sku_name            = "P1v2"
+  os_type             = "Windows"
+}
+
+resource "azurerm_windows_web_app" "mcitdevrm" {
+  name                = "mcitdevrm"
+  resource_group_name = azurerm_resource_group.mcitdevrm.name
+  location            = azurerm_service_plan.mcitdevrm.location
+  service_plan_id     = azurerm_service_plan.mcitdevrm.id
+
+  site_config {}
+}
