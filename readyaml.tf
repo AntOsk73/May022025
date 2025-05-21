2efec973eeb0ec9aabf3a2ffc78efc495d633b84
AntOsk73 <antoine.osky@bnc.ca>
2025-05-21 19:00:30 -0400

    Update readyaml.tf

--- readyaml.tf
+++ readyaml.tf
@@ -16,7 +16,7 @@ locals{
 resource "azurerm_service_plan" "batcha06sp" {
   for_each            ={for sp in local.linux_app_list: "${sp.name}"=>sp }
   name                = each.value.name
-  resource_group_name = azurerm_resource_group.azureresourcegroup.name
+  resource_group_name = azurerm_resource_group."antoineoskyform"
   location            = azurerm_resource_group."antoineoskyform"
   os_type             = each.value.os_type
   sku_name            = each.value.sku_name
