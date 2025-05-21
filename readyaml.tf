21c30bd48d4d245d4779e4eb9e432ff56483bf30
AntOsk73 <antoine.osky@bnc.ca>
2025-05-21 19:13:42 -0400

    Update readyaml.tf

--- readyaml.tf
+++ readyaml.tf
@@ -2,7 +2,7 @@ locals{
   linux_app=[for f in fileset("${path.module}/configs", "[^_]*.yaml") : yamldecode(file("${path.module}/configs/${f}"))]
   linux_app_list = flatten([
     for app in local.linux_app : [
-      for linuxapps in try(app.listoflinuxapp, []) :{
+      for linuxapps in try(app.listofVMs, []) :{
         name=linuxapps.name
         os_type=linuxapps.os_type
         sku_name=linuxapps.sku_name     
