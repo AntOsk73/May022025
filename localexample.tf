54bd1f1f66c7e4c821f1e80f9bdd488ac12f59ce
AntOsk73 <antoine.osky@bnc.ca>
2025-05-26 19:07:23 -0400

    Update localexample.tf

--- localexample.tf
+++ localexample.tf
@@ -56,7 +56,7 @@ vms = ["vm1", "vm2", "vm3"]
 
 
 regions = ["eastus", "westeurope", "southindia", "centralus"]
-us_regions = [for region in local.regions : region if list(region, "us")]
+  us_regions = [for r in local.regions : r if contains(r, "us")]
 }
 
 
