8c9f56ebafb66260d1e4138544f8f713108b7fc1
AntOsk73 <antoine.osky@bnc.ca>
2025-05-26 18:40:50 -0400

    Update level3.tf

--- level3.tf
+++ level3.tf
@@ -2,4 +2,9 @@ locals {
   rg_names = ["dev-rg", "test-rg", "prod-rg"]
 }
 
-for_each = toset(local.rg_names)
+resource "azurerm_resource_group" "rgs" {
+  for_each = toset(local.rg_names)
+
+  name     = each.value
+  location = "East US" 
+}
