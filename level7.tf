4f4029fa5d5ddb58be732144e585120132c1e206
AntOsk73 <antoine.osky@bnc.ca>
2025-05-26 19:30:25 -0400

    Update level7.tf

--- level7.tf
+++ level7.tf
@@ -1,19 +1,23 @@
+locals {
+  cidrs = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
 resource "azurerm_resource_group" "antoinelevel7" {
   name     = "example-resources"
   location = "West Europe"
 }
 
-resource "azurerm_virtual_network" "example" {
-  name                = "example-vnet"
+resource "azurerm_virtual_network" "antoinegroupname" {
+  name                = "antoinegroupname-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
+  location            = azurerm_resource_group.antoinegroup.location
   resource_group_name = azurerm_resource_group.example.name
 }
 
-resource "azurerm_subnet" "example" {
-  name                 = "example-subnet"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
+resource "azurerm_subnet" "antoinesubnet" {
+  name                 = "antoinesubnet-subnet"
+  resource_group_name  = azurerm_resource_group.antoinesubnet.name
+  virtual_network_name = azurerm_virtual_network.antoinesubnet.name
   address_prefixes     = ["10.0.1.0/24"]
 
   delegation {
