4d52392ed2124b58ac636df2f31397a19bb918ef
AntOsk73 <antoine.osky@bnc.ca>
2025-05-28 20:07:00 -0400

    Update level7.tf

--- level7.tf
+++ level7.tf
@@ -19,18 +19,21 @@ resource "azurerm_subnet" "antoinesubnet" {
   resource_group_name  = azurerm_resource_group.mcitlevel7.name
   virtual_network_name = azurerm_virtual_network.antoinesubnet.name
   address_prefixes     = ["10.0.1.0/24"]
+}
 
 resource "azurerm_subnet" "antoinesubnet" {
   name                 = "antoinesubnet1-subnet"
   resource_group_name  = azurerm_resource_group.mcitlevel7.name
   virtual_network_name = azurerm_virtual_network.antoinesubnet.name
   address_prefixes     = ["10.0.2.0/24"]
+}
 
 resource "azurerm_subnet" "antoinesubnet" {
   name                 = "antoinesubnet2-subnet"
   resource_group_name  = azurerm_resource_group.mcitlevel7.name
   virtual_network_name = azurerm_virtual_network.antoinesubnet.name
   address_prefixes     = ["10.0.3.0/24"]
+}
 
   delegation {
     name = "delegation"
