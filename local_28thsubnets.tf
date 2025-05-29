e2f0dcf9a4bbea14e7668f38c55462120607c2ac
AntOsk73 <antoine.osky@bnc.ca>
2025-05-28 20:13:23 -0400

    Delete local_28thsubnets.tf

--- local_28thsubnets.tf
+++
@@ -1,8 +0,0 @@
-locals {
-  region      = "eastus"
-  is_primary  = local.region == "eastus" ? true : false
-  environment = local.is_primary ? "production" : "staging"
-  subnet_data = yamldecode(file("${path.module}/may28/subnets.yaml"))
-
-resource "azurerm_subnet" "subnets" {
-  for_each    
