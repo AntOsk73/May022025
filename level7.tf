d82823798353ca40d9315a2edbe52d27974137ae
AntOsk73 <antoine.osky@bnc.ca>
2025-05-30 17:07:15 -0400

    Update level7.tf

--- level7.tf
+++ level7.tf
@@ -5,6 +5,6 @@ locals {
 resource "azurerm_virtual_network" "virtual_network" {
   name                = "level7-virtual_network"
   location            = "East US"
-  resource_group_name = "example-rg"
+  resource_group_name = "azurerm_resource_group.antoineoskyform.name"
   address_space       = ["10.0.0.0/16"]
 }
