22c46a54dd7e444837ddb078c8e6e7943df02999
AntOsk73 <antoine.osky@bnc.ca>
2025-05-30 17:31:09 -0400

    Update level8.tf

--- level8.tf
+++ level8.tf
@@ -22,3 +22,42 @@ resource "azurerm_subnet" "subnet_may30th" {
   virtual_network_name = azurerm_virtual_network" "virtual_network1.name
   address_prefixes     = ["10.0.1.0/24"]
 }
+
+resource "azurerm_network_interface" "nic" {
+  for_each = { for sp, name in local.vm_names : sp => name }
+
+  name                = "nic-${each.value}"
+  location            = azurerm_resource_group.antoineoskyform.location
+  resource_group_name = azurerm_resource_group.antoineoskyform.name
+
+  ip_configuration {
+    name                          = "ipconfig-${each.value}"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "vm_antoine" {
+  for_each = azurerm_network_interface.nic
+
+  name                = each.value.name
+  resource_group_name = azurerm_resource_group.antoineoskyform.name
+  location            = azurerm_resource_group.antoineoskyform.location
+  size                = "Standard_DS1_v2"
+  admin_username      = "adminuser"
+  admin_password      = "P@ssw0rd123!"
+
+  network_interface_ids = [each.value.id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+}
