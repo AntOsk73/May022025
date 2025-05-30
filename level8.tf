6255ba519768d1c91cd89c4a2c4711c6091efa49
AntOsk73 <antoine.osky@bnc.ca>
2025-05-30 17:26:22 -0400

    Update level8.tf

--- level8.tf
+++ level8.tf
@@ -15,3 +15,10 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
+
+resource "azurerm_subnet" "subnet_may30th" {
+  name                 = "antoine-subnet"
+  resource_group_name  = azurerm_resource_group.antoineoskyform.name
+  virtual_network_name = azurerm_virtual_network" "virtual_network1.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
