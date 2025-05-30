37a9a4aaa4eaeab4cb35c3c21dd857bc263e8760
AntOsk73 <antoine.osky@bnc.ca>
2025-05-30 17:10:25 -0400

    Update level7.tf

--- level7.tf
+++ level7.tf
@@ -8,3 +8,12 @@ resource "azurerm_virtual_network" "virtual_network" {
   resource_group_name = "azurerm_resource_group.antoineoskyform.name"
   address_space       = ["10.0.0.0/16"]
 }
+
+resource "azurerm_subnet" "subnet_testing" {
+  for_each = { for idx, cidr in local.cidrs : idx => cidr }
+
+  name                 = "subnet-${each.key}"
+  resource_group_name  = "azurerm_resource_group.antoineoskyform.name"
+  virtual_network_name = "azurerm_virtual_network.virtual_network.name"
+  address_prefixes     = [each.value]
+}
