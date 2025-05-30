8c6d6960aa5c52f650d54d0caa6d23c507cf6b2e
AntOsk73 <antoine.osky@bnc.ca>
2025-05-30 17:05:47 -0400

    Update level7.tf

--- level7.tf
+++ level7.tf
@@ -1,1 +1,10 @@
+locals {
+  cidrs = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
 
+resource "azurerm_virtual_network" "virtual_network" {
+  name                = "level7-virtual_network"
+  location            = "East US"
+  resource_group_name = "example-rg"
+  address_space       = ["10.0.0.0/16"]
+}
