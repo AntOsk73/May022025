d3ab5f1de7979b418ae59f74c54c72ed5872d13f
AntOsk73 <antoine.osky@bnc.ca>
2025-05-30 17:20:13 -0400

    Update level8.tf

--- level8.tf
+++ level8.tf
@@ -1,3 +1,10 @@
 locals {
   vm_names = ["webvm-1", "webvm-2", "webvm-3"]
 }
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "example-vnet"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+}
