2fd74af4996ebd43abd4d4fda32113eac39e3a84
AntOsk73 <antoine.osky@bnc.ca>
2025-06-02 19:53:05 -0400

    Update antoinefirewallmodule.tf

--- antoinefirewallmodule.tf
+++ antoinefirewallmodule.tf
@@ -1,6 +1,1 @@
-module "antoinefirewall" {
-  source              = "./modules/firewall_policy"
-  name                = "antoineokyfirewallpolicy"
-  resource_group_name = azurerm_resource_group.antoineoskyform.name
-  location            = azurerm_resource_group.antoineoskyform.location
-  }
+
