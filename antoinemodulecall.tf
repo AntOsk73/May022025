6906cc6b9bded5f3f5a5313787b6fa7ac49fc720
AntOsk73 <antoine.osky@bnc.ca>
2025-06-02 18:48:01 -0400

    Update antoinemodulecall.tf

--- antoinemodulecall.tf
+++ antoinemodulecall.tf
@@ -1,1 +1,14 @@
-
+module "antoinestorage" {
+  source              = "./modules/storage_account"
+  name                = "antoineokystorageaccount"
+  resource_group_name = azurerm_resource_group.mcitstudent.name
+  location            = azurerm_resource_group.mcitstudent.location
+  account_tier        = "Standard"
+  account_replication_type = "LRS"
+  kind                = "StorageV2"
+  access_tier         = "Hot"
+  tags = {
+    environment = "dev"
+    team        = "infra"
+  }
+}
