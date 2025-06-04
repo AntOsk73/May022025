d9ef23de4f481a0989fc2d62a97d88b256abcdd9
AntOsk73 <antoine.osky@bnc.ca>
2025-06-04 18:27:26 -0400

    Update local040625.tf

--- local040625.tf
+++ local040625.tf
@@ -2,7 +2,7 @@ resource "azurerm_storage_account" "antoinef0r_each" {
   for_each = var.storage_accounts
 
   name                     = each.key
-  resource_group_name      = "example-rg"
+  resource_group_name      = "antoineoskyform-rg"
   location                 = each.value
   account_tier             = "Standard"
   account_replication_type = "LRS"
