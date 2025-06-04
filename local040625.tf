a45346830ef9898fe256fedb4216b61f16a2ec56
AntOsk73 <antoine.osky@bnc.ca>
2025-06-04 19:00:46 -0400

    Update local040625.tf

--- local040625.tf
+++ local040625.tf
@@ -1,9 +1,50 @@
-resource "azurerm_storage_account" "antoinef0r_each" {
-  for_each = var.storage_accounts
+locals {
+  storage_accounts = {
+    sa1 = {
+      name                     = "samistorageacc1"
+      account_tier             = "Standard"
+      account_replication_type = "LRS"
+      kind                     = "StorageV2"
+      access_tier              = "Hot"
+    }
+    sa2 = {
+      name                     = "samistorageacc2"
+      account_tier             = "Standard"
+      account_replication_type = "GRS"
+      kind                     = "StorageV2"
+      access_tier              = "Cool"
+    }
+  }
+}
 
-  name                     = each.key
-  resource_group_name      = "antoineoskyform-rg"
-  location                 = each.value
-  account_tier             = "Standard"
+module "mymcitstorage" {
+  for_each = local.storage_accounts
+
+  source              = "./modules/storage_account"
+  name                = each.value.name
+  resource_group_name = azurerm_resource_group.mcit420zz5um.name
+  location            = azurerm_resource_group.mcit420zz5um.location
+  account_tier        = each.value.account_tier
+  account_replication_type = each.value.account_replication_type
+  kind                = each.value.kind
+  access_tier         = each.value.access_tier
+  tags = {
+    environment = "dev"
+    team        = "infra"
+  }
+}
+
+module "myforeachmcitstorage" {
+  source              = "./modules/storage_account"
+  name                = "samistorageacc12345"
+  resource_group_name = azurerm_resource_group.mcit420zz5um.name
+  location            = azurerm_resource_group.mcit420zz5um.location
+  account_tier        = "Standard"
   account_replication_type = "LRS"
+  kind                = "StorageV2"
+  access_tier         = "Hot"
+  tags = {
+    environment = "dev"
+    team        = "infra"
+  }
 }
