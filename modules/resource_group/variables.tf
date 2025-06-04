77112e59ad7ee88c842537f40b4da5ad7bb65d39
AntOsk73 <antoine.osky@bnc.ca>
2025-06-04 19:41:41 -0400

    Update variables.tf

--- modules/resource_group/variables.tf
+++ modules/resource_group/variables.tf
@@ -1,1 +1,9 @@
+variable "resource_group_name" {
+  description = "The name of the resource group."
+  type        = string
+}
 
+variable "location" {
+  description = "Azure region where the storage account will be created."
+  type        = string
+}
