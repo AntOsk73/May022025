644c10cc487b9256ed21f169e2fcd568dbb62377
AntOsk73 <antoine.osky@bnc.ca>
2025-06-04 19:04:14 -0400

    Update local040625.tf

--- local040625.tf
+++ local040625.tf
@@ -22,8 +22,8 @@ module "mystorage" {
 
   source              = "./modules/storage_account"
   name                = each.value.name
-  resource_group_name = azurerm_resource_group.mcit420zz5um.name
-  location            = azurerm_resource_group.mcit420zz5um.location
+  resource_group_name = azurerm_resource_group.antoineoskyform.name
+  location            = azurerm_resource_group.antoineoskyform.location
   account_tier        = each.value.account_tier
   account_replication_type = each.value.account_replication_type
   kind                = each.value.kind
@@ -37,8 +37,8 @@ module "mystorage" {
 module "myforeachmcitstorage" {
   source              = "./modules/storage_account"
   name                = "samistorageacc12345"
-  resource_group_name = azurerm_resource_group.mcit420zz5um.name
-  location            = azurerm_resource_group.mcit420zz5um.location
+  resource_group_name = azurerm_resource_group.antoineoskyform.name
+  location            = azurerm_resource_group.antoineoskyform.location
   account_tier        = "Standard"
   account_replication_type = "LRS"
   kind                = "StorageV2"
